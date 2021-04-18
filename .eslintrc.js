48a231b475f3787f272e9679f767f532cdeda5e1
David Mosca <david.mosca69@gmail.com>
2021-04-18 13:01:26 +0200

    add config for prettier abd eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  overrides: [
+    {
+      files: ['**/*.spec.js', '**/*.spec.jsx', '**/*.test.js', '**/*.test.jsx'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+  },
+};
